47df0c9fa2e8cc6a1a4e2c8be8b5d1cf6d80779d
JJBINY <97151887+JJBINY@users.noreply.github.com>
2023-09-18 18:14:15 +0900

    Revert "[Test] #68-task-1 암장명 자동완성 테스트"
    This reverts commit 883a4c607578cea81903e72d725816a7186e68cf.

--- src/test/java/swm/s3/coclimb/api/RestDocsTestSupport.java
+++ src/test/java/swm/s3/coclimb/api/RestDocsTestSupport.java
@@ -1,6 +1,7 @@
 package swm.s3.coclimb.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -20,4 +21,12 @@ public abstract class RestDocsTestSupport extends IntegrationTestSupport{
     protected ObjectMapper objectMapper;
     @Autowired
     protected JwtManager jwtManager;
+
+    @AfterEach
+    void clearDB() {
+        gymLikeJpaRepository.deleteAllInBatch();
+        mediaJpaRepository.deleteAllInBatch();
+        gymJpaRepository.deleteAllInBatch();
+        userJpaRepository.deleteAllInBatch();
+    }
 }
