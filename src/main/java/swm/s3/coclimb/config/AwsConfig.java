9467be38bd0fd518cfe95be98684cd314bd79588
JJBINY <sisoya0424@gmail.com>
2023-10-14 02:49:08 +0900

    [Refactor] #93-task-4 AwsConfig, RdsProperties

--- src/main/java/swm/s3/coclimb/config/AwsConfig.java
+++ src/main/java/swm/s3/coclimb/config/AwsConfig.java
@@ -2,9 +2,13 @@ package swm.s3.coclimb.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -12,13 +16,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
 import software.amazon.awssdk.services.sts.StsClient;
-import software.amazon.awssdk.services.sts.StsClientBuilder;
+import swm.s3.coclimb.api.adapter.out.aws.AwsCloudFrontManager;
+import swm.s3.coclimb.config.propeties.AwsCloudFrontProperties;
 import swm.s3.coclimb.config.propeties.AwsCredentialsProperties;
 import swm.s3.coclimb.config.propeties.AwsRdsProperties;
 
@@ -61,7 +65,7 @@ public class AwsConfig {
     @Bean
     @Profile("release")
     public DataSource dataSource() {
-        AwsRdsProperties.RdsSecret secret = rdsProperties.getSecret();
+        RdsSecret secret = getRdsSecret();
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setUrl(rdsProperties.getDbUrl());
         dataSource.setUsername(secret.getUsername());
